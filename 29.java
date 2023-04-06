05ffa2e4921df40c665e7014b48f61705b146012
Deepakkushwahg <deepakkushwahg@gmail.com>
2023-04-06 23:18:36 +0530

    29.java

---
+++ 29.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend==1<<31 && divisor==-1) return Integer.MAX_VALUE;
+        boolean isNegative = (dividend<0)^(divisor<0)? true : false;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        int quotient = 0;
+        while(dividend-divisor >=0){
+            int count = 0;
+            while(dividend - (divisor<<1<<count) >=0) count++;
+            quotient += 1<<count;
+            dividend -= divisor<<count;
+        }
+        return isNegative? -quotient : quotient;
+    }
+}

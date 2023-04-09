eb74e27fbef40fd78fa9f450df08cb8e3a66883c
Deepakkushwahg <deepakkushwahg@gmail.com>
2023-04-09 23:32:13 +0530

    78.java

---
+++ 78.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        backtrace(nums,0,new ArrayList<Integer>(),ans);
+        return ans;
+    }
+    private void backtrace(int[] nums,int start,List<Integer> track,List<List<Integer>> ans){
+        ans.add(new ArrayList<>(track));
+        for(int i=start;i<nums.length;i++){
+            track.add(nums[i]);
+            backtrace(nums,i+1,track,ans);
+            track.remove(track.size()-1);
+        }
+    }
+}
